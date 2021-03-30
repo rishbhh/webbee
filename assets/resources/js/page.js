d9f3de565620be49bee132eb798ede1fe6f06e01
rishbhh <40095968+rishbhh@users.noreply.github.com>
2021-03-30 14:28:35 +0530

    Changes

--- assets/resources/js/page.js
+++ assets/resources/js/page.js
@@ -1,6 +1,26 @@
 
-document.onreadystatechange = function () {
-    if (document.readyState == "interactive") {
-        
-    }
-};
+
+$(document).ready(function () {
+    $('.customer-logos').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: true,
+        dots: false,
+        pauseOnHover: false,
+        prevArrow: '<i class="slick-prev fas fa-angle-left"></i>',
+        nextArrow: '<i class="slick-next fas fa-angle-right"></i>',
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 3
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 2
+            }
+        }]
+    });
+});
